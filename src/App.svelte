6bd2c01aff7bb61fef18c22e37292d715f1d142b
Em Zhan <zqianem@gmail.com>
2020-07-17 10:26:08 -0400

    Add basic keyboard controls

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,43 @@
 <script>
   import Map from './map/Map.svelte';
+
+  let longitude = 40;
+  let latitude = 0;
+  let zoom = 2;
+
+  const speed = 1;
+
+  function handleKeydown(event) {
+    switch(event.key) {
+      case 'ArrowUp':
+      case 'w':
+        latitude += speed;
+        break;
+      case 's':
+      case 'ArrowDown':
+        latitude -= speed;
+        break;
+      case 'd':
+      case 'ArrowRight':
+        longitude += speed;
+        break;
+      case 'a':
+      case 'ArrowLeft':
+        longitude -= speed;
+        break;
+    }
+  }
 </script>
 
 <main>
-  <Map/>
+  <Map
+    bind:longitude
+    bind:latitude
+    bind:zoom
+  />
 </main>
+
+<svelte:window on:keydown={handleKeydown}/>
 
 <style>
   main {
