f9d20f1c019f2b3ca79173dfcceea1abf8def96b
Em Zhan <zqianem@gmail.com>
2020-07-29 18:04:55 -0400

    Refactor common clipping to rotate method

--- src/map/projections/equal-earth/forward.glsl
+++ src/map/projections/equal-earth/forward.glsl
@@ -7,14 +7,9 @@ const float A3 = 0.000893;
 const float A4 = 0.003796;
 const float M = sqrt(3.0) / 2.0;
 
-const float PI = radians(180.0);
-
 void equalEarth(inout vec2 coord, in vec2 lonLat0, in vec2 lonLat,
     out bool clip) {
-  rotate(lonLat0, lonLat);
-
-  float distanceFromMeridian = abs(lonLat.x); // coordinates already rotated
-  clip = distanceFromMeridian > PI - radians(1.0);
+  rotate(lonLat0, lonLat, clip);
 
   float l = asin(M * sin(lonLat.y));
   float l2 = l * l;
--- src/map/projections/rotate/forward.glsl
+++ src/map/projections/rotate/forward.glsl
@@ -1,10 +1,15 @@
 #pragma glslify: export(rotate)
 
+const float PI = radians(180.0);
+
 // Rotates longitude/latitude points on a sphere so that lonLat0 becomes the
 // center where (0,0) was and rest of the points follow accordingly
 //
 // based on https://gis.stackexchange.com/questions/10808/manually-transforming-rotated-lat-lon-to-regular-lat-lon
-void rotate(in vec2 lonLat0, inout vec2 lonLat) {
+//
+// clips coordinate if it is too close to edge of rotated area to prevent lines
+// from wrapping across map
+void rotate(in vec2 lonLat0, inout vec2 lonLat, out bool clip) {
   vec3 xyzCoord = vec3(
     cos(lonLat.x) * cos(lonLat.y),
     sin(lonLat.x) * cos(lonLat.y),
@@ -23,4 +28,7 @@ void rotate(in vec2 lonLat0, inout vec2 lonLat) {
   xyzCoord = rotateLatitude * rotateLongitude * xyzCoord;
 
   lonLat = vec2(atan(xyzCoord.y, xyzCoord.x), asin(xyzCoord.z));
+
+  float distanceFromMeridian = abs(lonLat.x);
+  clip = distanceFromMeridian > PI - radians(1.0);
 }
