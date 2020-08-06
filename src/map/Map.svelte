34a67ce4ffbe55fca48bf73391e621f2967d32c1
Em Zhan <zqianem@gmail.com>
2020-08-06 18:26:53 -0400

    Implement a color scale
    Mostly temporary, will want to extract this and other data into some
    JSON file

--- src/map/Map.svelte
+++ src/map/Map.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import * as twgl from 'twgl.js';
   import * as topojson from 'topojson-client';
+  import * as chroma from 'chroma-js';
 
   import griddedVertexShader from './gridded.vert';
   import griddedFragmentShader from './gridded.frag';
@@ -67,8 +68,29 @@
     let data = new Float32Array(buffer);
     let texture = new Uint8Array(buffer);
 
+    let f = chroma.scale([
+      '#00429d',
+      '#2e59a8',
+      '#4771b2',
+      '#5d8abd',
+      '#73a2c6',
+      '#8abccf',
+      '#a5d5d8',
+      '#c5eddf',
+      '#ffffe0'
+    ]).mode('lrgb').domain([240, 320]);
+
+    for (let i = 0; i < data.length; i++) {
+      let color = f(data[i]).rgb();
+      texture[4*i+0] = color[0];
+      texture[4*i+1] = color[1];
+      texture[4*i+3] = color[2];
+      texture[4*i+3] = 255;
+    }
+
     griddedTexture = twgl.createTexture(gl, {
       src: texture,
+      mag: gl.NEAREST, // show zoomed in data as individual pixels
       min: gl.LINEAR,
       width: griddedTextureInfo.width,
       height: griddedTextureInfo.height,
