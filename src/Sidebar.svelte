ec1e5581fc478007c2f34d10bd8d3a95b9b1ec85
Em Zhan <zqianem@gmail.com>
2020-09-19 17:45:16 -0400

    Add menu button to sidebar
    Opens the Demo menu for now, but will want this to open a SMUI modal
    drawer eventually.

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -1,5 +1,9 @@
 <script>
-  import IconButton, {Icon} from '@smui/icon-button';
+  import IconButton from '@smui/icon-button';
+
+  // not using { Icon } import syntax for significantly faster build times, as
+  // recommended in the carbon-icons-svelte README
+  import Menu32 from "carbon-icons-svelte/lib/Menu32";
 
   export let openedMenu;
 
@@ -12,22 +16,32 @@
   }
 </script>
 
-<nav on:click={toggleDemo}></nav>
+<nav>
+  <IconButton on:click={toggleDemo}>
+    <Menu32 class="icon"/>
+  </IconButton>
+</nav>
 
 <style>
   nav {
-    background: grey;
-    width: 3.5em;
+    background: #202124;
     z-index: 2;
-    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em;
+  }
+
+  :global(svg.icon) {
+    fill: white;
   }
 
   @media (max-width: 36rem) {
     nav {
+      flex-direction: row;
+      background: transparent;
       position: absolute;
       height: 3.5em;
       width: 100%;
-      opacity: 0.5;
     }
   }
 </style>
