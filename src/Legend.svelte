2833872b54298f5ea9a3d34484101885db2962b8
Em Zhan <zqianem@gmail.com>
2020-10-11 16:31:55 -0400

    Add a labeled scale to the colormap legend
    Still no units yet...
    
    d3-scale and d3-selection are soft dependencies for d3-axis

--- src/Legend.svelte
+++ src/Legend.svelte
@@ -1,6 +1,24 @@
 <script>
+  import * as d3 from 'd3-selection';
+  import { axisBottom } from 'd3-axis';
+  import { scaleLinear } from 'd3-scale';
+  import { onMount } from 'svelte';
+
   export let griddedColormap;
-  export let griddedDomain;
+  export let griddedDomain = [0, 0];
+
+  let svgScale;
+  let svgScaleWidth;
+  function createAxis() {
+    let axis = axisBottom(
+      scaleLinear()
+        .domain(griddedDomain)
+        .range([-0.5, svgScaleWidth - 0.5])
+    );
+    d3.select(svgScale).call(axis);
+  }
+  onMount(createAxis);
+  $: svgScaleWidth, griddedDomain, createAxis();
 
   let cssLut;
   $: cssLut = griddedColormap.lut.map(colorArray => {
@@ -13,7 +31,9 @@
   <div
     class="legend"
     style="background: linear-gradient(to right, {cssLut});"
+    bind:clientWidth={svgScaleWidth}
   ></div>
+  <svg bind:this={svgScale}></svg>
 </div>
 
 <style>
@@ -22,11 +42,13 @@
     flex-direction: column;
     justify-content: flex-end;
     align-items: center;
+    user-select: none;
   }
 
   div.legend-wrapper > * {
     max-width: 90%;
-    width: 30em;
+    width: 30rem;
+    z-index: 1;
   }
 
   p {
@@ -36,6 +58,13 @@
 
   div.legend {
     height: 2em;
-    margin-bottom: 2em;
+  }
+
+  svg {
+    font-size: 0.875em;
+    overflow: visible;
+    color: white;
+    height: 3em;
+    margin-bottom: 2rem;
   }
 </style>
