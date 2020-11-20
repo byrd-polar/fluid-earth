e6e4dfbd6fe48064ab94f603497c42e830b3fc66
Em Zhan <zqianem@gmail.com>
2020-11-19 22:11:59 -0500

    Add script to create dataset inventory
    For determining file sizes and valid date ranges for datasets

---
+++ data/inventory.js
@@ -0,0 +1,29 @@
+import { writeFile } from 'fs/promises';
+import { OUTPUT_DIR } from './helpers.js';
+import path from 'path';
+
+const gfsSharedValues = {
+  bytesPerFile: '2076480',
+  start: '2020-11-01T00:00Z',
+  intervalInHours: 3,
+}
+
+const inventoryJSON = {
+  topology: {
+    lastUpdated: '2018-05-21',
+    bytes: '1207774',
+  },
+  'gfs-0p25-temperature-surface': gfsSharedValues,
+  'gfs-0p25-u-wind-velocity-10m': gfsSharedValues,
+  'gfs-0p25-v-wind-velocity-10m': gfsSharedValues,
+  'gfs-0p25-wind-speed-10m': gfsSharedValues,
+}
+
+export async function inventory() {
+  gfsSharedValues.end = '2020-11-14T12:00Z'; // hardcoded for now
+
+  let outputFile = path.join(OUTPUT_DIR, 'inventory.json');
+  await writeFile(outputFile, JSON.stringify(inventoryJSON));
+}
+
+inventory();
