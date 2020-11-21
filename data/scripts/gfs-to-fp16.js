e0e8b39f970f39d1f719e6bf01a94063f263dc83
Em Zhan <zqianem@gmail.com>
2020-11-21 00:27:59 -0500

    Add shell script for converting GFS to fp16
    Should even work on Windows!

---
+++ data/scripts/gfs-to-fp16.js
@@ -0,0 +1,38 @@
+#!/bin/env node
+// Convert a GFS file to fp16
+//
+// Usage: node gfs-to-fp16.js [inputFile] [outputFile]
+
+import { Float16Array } from '@petamoriken/float16';
+import { spawn } from 'child_process';
+import { Transform } from 'stream';
+import { createWriteStream } from 'fs';
+import { platform } from 'os';
+
+if (process.argv.length != 2 + 2) {
+  console.log('wrong number of arguments, expected 2');
+  process.exit(1);
+}
+
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
+
+const wgrib2 = spawn('wgrib2', [
+  inputFile,
+  '-inv', platform() === 'win32' ? 'NUL' : '/dev/null',
+  '-bin', '-',
+  '-no_header',
+  '-order', 'we:ns'
+]).stdout;
+
+const float16 = new Transform({
+  transform(chunk, _encoding, callback) {
+    const original = new Float32Array(chunk.buffer);
+    const converted = new Float16Array(original);
+    callback(null, Buffer.from(converted.buffer)); 
+  }
+});
+
+const output = createWriteStream(outputFile);
+
+wgrib2.pipe(float16).pipe(output);
