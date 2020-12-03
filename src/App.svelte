bb11df16791dcb5f43de0eb6037cc8f4cd1720aa
Michael Gravina <gravina.2@osu.edu>
2020-12-03 12:01:35 -0500

    Made top-bar logo links clickable

--- src/App.svelte
+++ src/App.svelte
@@ -184,16 +184,25 @@
       {griddedDomain}
       {particleDisplay}
     />
-    <div class = "title" style = 'pointer-events: none;'>
+    <div class = "title">
       <p>Fluid Earth Viewer<p>
     </div>
 
    <div class = "logos" >
-    <AddComment32 width = "35" height = "35"/>&nbsp;
-    <img src = "NSF-logo-white-small.png" width = "35" height = "35"
-         alt="National Science Foundation logo">&nbsp;
-    <img src = "OSU-logo-white-small.png" width = "30" height = "35"
-         alt="Ohio State University logo">
+    <a class = "nav-link tooltip" href="https://fever.byrd.osu.edu/feedback.html" target="_blank" alt="Leave feedback">
+      <AddComment32 width = "35" height = "35"/>&nbsp;
+      <span class="tooltiptext">Feedback</span>
+    </a>
+    <a class = "nav-link tooltip" href="https://www.nsf.gov/funding/pgm_summ.jsp?pims_id=504793" target="_blank">
+      <img src = "NSF-logo-white-small.png" width = "35" height = "35"
+           alt="National Science Foundation logo">&nbsp;
+      <span class="tooltiptext">NSF AISL</span>
+    </a>
+    <a class = "nav-link tooltip" href="https://bpcrc.osu.edu/" target="_blank">
+      <img src = "OSU-logo-white-small.png" width = "30" height = "35"
+           alt="Ohio State University logo">
+      <span class="tooltiptext">OSU BPCRC</span>
+    </a>
   </div>
 
 
@@ -222,7 +231,7 @@
 
   @media (max-width: 750px) {
     .title {
-        display: none;
+      display: none;
     }
     .logos{
       display: none;
@@ -241,11 +250,12 @@
     /* width: 500px; */
     margin-left: auto;
   }
+
   .title {
    color: #D3D3D3;
     font-family: Quicksand-bold;
     font-size: 1.2rem;
-    pointer-events: auto;
+    pointer-events: none;
     font-weight: bolder;
     margin-left: .75rem;
   }
@@ -256,4 +266,51 @@
     margin-right: 0rem;
     margin-top: .50rem;
   }
+  .nav-link{
+    pointer-events: auto;
+    color: white;
+    text-decoration: none;
+  }
+  .nav-link:link{
+    color: white;
+    text-decoration: none;
+  }
+  .nav-link:visited{
+    color: white;
+    text-decoration: none;
+  }
+
+
+
+  .tooltip {
+	  position: relative;
+	  display: inline-block;
+	  border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+	}
+
+	/* Tooltip text */
+	.tooltip .tooltiptext {
+	  visibility: hidden;
+	  background-color: black;
+	  font-size: small;
+	  color: #fff;
+	  text-align: center;
+	  padding: 5px 0;
+	  border-radius: 6px;
+
+	  /* Position the tooltip text - see examples below! */
+	  position: absolute;
+	  z-index: 1;
+	  top: 105%;
+	  right: 50%;
+	}
+
+	/* Show the tooltip text when you mouse over the tooltip container */
+	.tooltip:hover .tooltiptext {
+	  visibility: visible;
+	}
+
+
+
+
 </style>
