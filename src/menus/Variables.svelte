3fc524f73af9e39821f881f1cb8431e96670f6a4
Adelyn Arens <arens.28@buckeyemail.osu.edu>
2020-12-14 13:07:13 -0500

    Add Weather and Climate mode toggle
    with function to connect to variables when they become available

--- src/menus/Variables.svelte
+++ src/menus/Variables.svelte
@@ -15,8 +15,28 @@
   export let particlesShown;
 
   let iconStyle = "fill: #0ff !important;";
+
+  function selectMode(mode) {
+    if(mode == 'on') {
+      mode = 'weather';
+    }
+    if(mode == 'off') {
+      mode = 'climate'
+    }
+  }
 </script>
 
+
+
+<!-- Mode Toggle -->
+<div class="onoffswitch" darkBackground = "true">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" tabindex="0" checked >
+    <label class="onoffswitch-label" for="myonoffswitch">
+        <span class="onoffswitch-inner" on:click={() => selectMode('on')}></span>
+        <span class="onoffswitch-switch" on:click={() => selectMenuItem('off')}>  </span>
+    </label>
+</div>
+<!-- end of mode toggle -->
 
 <h2>ATMOSPHERE</h2>
 <ul>
@@ -175,4 +195,81 @@
   .selected {
     color: gold;
   }
+  .onoffswitch {
+    position: relative;
+    width: 150px;
+    -webkit-user-select:none;
+    -moz-user-select:none;
+   -ms-user-select: none;
+}
+.onoffswitch-checkbox {
+    position: absolute;
+    opacity: 0;
+    pointer-events: none;
+}
+.onoffswitch-label {
+    display: block;
+    overflow: hidden;
+    cursor: pointer;
+    border: 2px solid #999999;
+    border-radius: 20px;
+}
+.onoffswitch-inner {
+    display: block;
+    width: 200%;
+    margin-left: -100%;
+    transition: margin 0.3s ease-in 0s;
+}
+.onoffswitch-inner:before, .onoffswitch-inner:after {
+    display: block;
+    float: left;
+    width: 50%;
+    height: 30px;
+    padding: 0;
+    line-height: 30px;
+    font-size: 17px;
+    color: white;
+    box-sizing: border-box;
+}
+.onoffswitch-inner:before {
+    content: "Weather Mode";
+    font-family: Quicksand-regular;
+    padding-left: 10px;
+    color: #FFFFFF;
+    font-size: 1rem;
+    font-weight: 400;
+    color: hsla(0, 0%, 100%, .8);
+}
+.onoffswitch-inner:after {
+    content: "Climate Mode";
+    font-family: Quicksand-regular;
+    padding-right: 10px;
+    color: #FFFFFF;
+    text-align: right;
+    font-size: 1rem;
+    font-weight: 400;
+    color: hsla(0, 0%, 100%, .8);
+}
+.onoffswitch-switch {
+    display: block;
+    width: 18px;
+    margin: 6px;
+    background: white;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 122px;
+    border: 2px
+    solid #999999;
+    border-radius: 20px;
+    transition: all 0.3s ease-in 0s;
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
+    margin-left: 0;
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
+    right: 0px;
+}
+
+
 </style>
