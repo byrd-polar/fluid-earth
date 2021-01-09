7e450b2acbc54bb773a720745de89ae02489bc63
Em Zhan <zqianem@gmail.com>
2021-01-09 17:54:54 -0500

    Reduce tag and indent clutter in About.svelte

--- src/menus/About.svelte
+++ src/menus/About.svelte
@@ -1,106 +1,100 @@
+<!-- Note: this file intentionally omits optional closing html tags such as
+</p>, </dt>, and </dd> to reduce some of the tag clutter. -->
+
 <h2>The Project</h2>
 <p>
-  Fluid Earth Viewer (FEVer) is an interactive web application that allows you
-  to visualize current and past conditions of Earth's atmosphere and oceans.
-</p>
+Fluid Earth Viewer (FEVer) is an interactive web application that allows you to
+visualize current and past conditions of Earth's atmosphere and oceans.
 <p>
-  You can use FEVer to learn about the atmosphere and oceans by exploring the
-  daily conditions in places where you live, work, and play or examining whole
-  regions of the planet over years. In particular, FEVer provides hands-on
-  visualizations of conditions in polar regions, changes they are undergoing,
-  and connections between polar regions and the rest of the planet. Built on an
-  open-source application, FEVer is a vehicle for modern Earth science
-  communication, making information used by the scientific community accessible
-  and engaging to everyone. FEVer is explorable 24 hours a day, 7 days a week
-  using your computer, tablet, and smartphone.
-</p>
+You can use FEVer to learn about the atmosphere and oceans by exploring the
+daily conditions in places where you live, work, and play or examining whole
+regions of the planet over years. In particular, FEVer provides hands-on
+visualizations of conditions in polar regions, changes they are undergoing, and
+connections between polar regions and the rest of the planet. Built on an
+open-source application, FEVer is a vehicle for modern Earth science
+communication, making information used by the scientific community accessible
+and engaging to everyone. FEVer is explorable 24 hours a day, 7 days a week
+using your computer, tablet, and smartphone.
 <p>
-  Our project team is working to remove obstacles to learning about our dynamic
-  planet, Earth!
-</p>
+Our project team is working to remove obstacles to learning about our dynamic
+planet, Earth!
 
 <h2>Our Sponsor</h2>
 <p>
-  This project is sponsored by the U.S. National Science Foundation as an
-  Exploratory Pathways project under the Advancing Informal STEM Learning (AISL)
-  Program.
-</p>
+This project is sponsored by the U.S. National Science Foundation as an
+Exploratory Pathways project under the Advancing Informal STEM Learning (AISL)
+Program.
 
 <h2>The Team</h2>
 <p>
-  The team is comprised of multi-disciplinary researchers and educators from The
-  Ohio State University committed to improving Earth Science and learning
-  strategies in diverse education settings.
-</p>
+The team is comprised of multi-disciplinary researchers and educators from The
+Ohio State University committed to improving Earth Science and learning
+strategies in diverse education settings.
 <p>
-  Members from OSU's Byrd Polar and Climate Research Center (BPCRC) bring their
-  expertise in education and outreach, polar weather and climate, and computer
-  information sciences. The team is also fortunate to have a member from OSU's
-  School of Communication with a wealth of knowledge on user-testing and the
-  effects and implications of new media technologies and virtual environments.
-  Together, this team has extensive experience communicating with public
-  audiences of all ages.
-</p>
+Members from OSU's Byrd Polar and Climate Research Center (BPCRC) bring their
+expertise in education and outreach, polar weather and climate, and computer
+information sciences. The team is also fortunate to have a member from OSU's
+School of Communication with a wealth of knowledge on user-testing and the
+effects and implications of new media technologies and virtual environments.
+Together, this team has extensive experience communicating with public audiences
+of all ages.
 <dl>
-  <dt>Jason Cervenec (PI)</dt>
-  <dd>BPCRC Education &amp; Outreach Director</dd>
-  <dd>Oversees project and creation and field-testing of learning
-    experiences</dd>
+  <dt>Jason Cervenec (PI)
+  <dd>BPCRC Education &amp; Outreach Director
+  <dd>Oversees project and creation and field-testing of learning experiences
 
-  <dt>Jesse Fox</dt>
-  <dd>School of Communication Associate Professor</dd>
-  <dd>Oversees user-testing and virtual environment settings</dd>
+  <dt>Jesse Fox
+  <dd>School of Communication Associate Professor
+  <dd>Oversees user-testing and virtual environment settings
 
-  <dt>Bidhyananda Yadav</dt>
-  <dd>BPCRC Research Associate</dd>
+  <dt>Bidhyananda Yadav
+  <dd>BPCRC Research Associate
   <dd>Oversees code development and collaborates with J. Cervenec on
-    field-testing and learning experiences</dd>
+    field-testing and learning experiences
 
-  <dt>Aaron Wilson</dt>
-  <dd>BPCRC Senior Research Associate</dd>
+  <dt>Aaron Wilson
+  <dd>BPCRC Senior Research Associate
   <dd>Collaborates with B. Yadav on code development and with J. Cervenec on
-    field-testing and learning experiences</dd>
+    field-testing and learning experiences
 
-  <dt>Michael Gravina</dt>
-  <dd>Front-End Developer</dd>
+  <dt>Michael Gravina
+  <dd>Front-End Developer
   <dd>Collaborates with B. Yadav on code development and with J. Cervenec on
-    field-testing and learning experiences</dd>
+    field-testing and learning experiences
 </dl>
 <p>
-  In addition, the team depends on a talented group of undergraduates and
-  Research Experience for Undergraduates (REU) students working at The Ohio
-  State University to assist in design of the user interface, conducting of
-  user-testing, and organization of data: Bingyu (Sophia) Li, Dingyu Hu, Ruiyang
-  Chang, Joey Wong, Craig Bossley, Michael (Em) Zhan, Mimi Cai, and Adelyn
-  Arens. Em Zhan is spearheading the reimplementation of FEVer in a WebGL-based
-  framework.
-</p>
+In addition, the team depends on a talented group of undergraduates and Research
+Experience for Undergraduates (REU) students working at The Ohio State
+University to assist in design of the user interface, conducting of
+user-testing, and organization of data: Bingyu (Sophia) Li, Dingyu Hu, Ruiyang
+Chang, Joey Wong, Craig Bossley, Michael (Em) Zhan, Mimi Cai, and Adelyn Arens.
+Em Zhan is spearheading the reimplementation of FEVer in a WebGL-based
+framework.
 
 <h2>Data Sources</h2>
 <p>
-  FEVer utilizes an enormous amount of scientifically complex data, allowing you
-  to explore the interconnectedness of weather and climate, including between
-  polar regions and the rest of the planet.
-</p>
-<p>Data sets include:</p>
+FEVer utilizes an enormous amount of scientifically complex data, allowing you
+to explore the interconnectedness of weather and climate, including between
+polar regions and the rest of the planet.
+<p>
+Data sets include:
 <dl>
-  <dt>Weather</dt>
-  <dd>GFS (Global Forecast System)</dd>
+  <dt>Weather
+  <dd>GFS (Global Forecast System)
 
-  <dt>Ocean Currents</dt>
-  <dd>OSCAR (Ocean Surface Current Analysis Real-time)</dd>
+  <dt>Ocean Currents
+  <dd>OSCAR (Ocean Surface Current Analysis Real-time)
 
-  <dt>Sea Surface Temperature</dt>
-  <dd>RTGSST (Real Time Global Sea Surface Temperature)</dd>
+  <dt>Sea Surface Temperature
+  <dd>RTGSST (Real Time Global Sea Surface Temperature)
 
-  <dt>Aerosols and Chemistry</dt>
-  <dd>GEOS-5 (Goddard Earth Observing System, Version 5)</dd>
+  <dt>Aerosols and Chemistry
+  <dd>GEOS-5 (Goddard Earth Observing System, Version 5)
 </dl>
 
 <h2>Terms of Use</h2>
 <p>
 Copyright 2021 The Ohio State University
-</p>
 <p>
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -108,11 +102,9 @@ the Software without restriction, including without limitation the rights to
 use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
-</p>
 <p>
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-</p>
 <p>
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
@@ -120,11 +112,9 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-</p>
 <p>
 The laws of the State of Ohio shall govern these Terms of Use and any disputes
 relating to our site.
-</p>
 <p>
 IF YOU DO NOT AGREE TO AND ACCEPT THESE TERMS OF USE YOU SHOULD NOT USE THE
 SOFTWARE.
