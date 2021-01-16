4a7e284f69a4cdebeb12b362e7f51f1e809bfe5a
Em Zhan <zqianem@gmail.com>
2021-01-16 13:05:20 -0500

    Add toggle focus styles and contrast
    Background now complies with 3:1 contrast ratio WCAG AA for graphical
    elements and focus highlight replaces default focus outline

--- src/components/HeaderToggle.svelte
+++ src/components/HeaderToggle.svelte
@@ -8,7 +8,8 @@
   <Toggle
     bind:toggled={menusDetailed}
     label="Advanced options"
-    toggledColor="#676778"
+    toggledColor="#40B598"
+    untoggledColor="#ABABAB"
     style="margin-left: auto"
   />
 </div>
@@ -20,5 +21,37 @@
 
   div :global(label) {
     text-align: right;
+    color: white;
+  }
+
+  div :global(button::after) {
+	  position: absolute;
+	  content: "";
+	  top: 0;
+	  bottom: 0;
+	  left: -0.75rem;
+	  margin: auto;
+	  height: 2.5rem;
+	  width: 2.5rem;
+	  text-align: center;
+	  border-radius: 50%;
+	  background-color: transparent;
+	  transition: transform 150ms ease-out;
+  }
+
+  div :global(button[aria-checked="true"]::after) {
+    transform: translateX(1.375rem);
+  }
+
+  div :global(button:focus) {
+    outline: none;
+  }
+
+  div :global(button:hover::after) {
+    background-color: rgba(255, 255, 255, 0.05);
+  }
+
+  div :global(button:focus::after) {
+    background-color: rgba(255, 255, 255, 0.15);
   }
 </style>
