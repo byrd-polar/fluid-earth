byrd-polar/fluid-earth

commit ba15a95f9b5a2e1306a17b04cfcdb7eb89e87847

Make it easier to search for multiple locations
Don't have to refocus the search box. A bit awkward as to when the
dropdown appears with the top search results when there is no input
(just the most populous cities).

src/components/SearchBox.svelte
@@ -10,9 +10,13 @@
   let data = undefined;
   let focused = false;
   let loading = false;
-  $: dropdownShown = focused && (value.length > 0 || !loading);
+  let dropdownTemporarilyHidden = false;
+  $: dropdownShown = focused && (value.length > 0 || !loading) &&
+    !dropdownTemporarilyHidden;
 
   async function filterOptions() {
+    dropdownTemporarilyHidden = false;
+
     if (loading) return;
 
     if (!data) {
@@ -31,8 +35,8 @@
   }
 
   function select(option) {
-    inputElement.blur();
     value = '';
+    dropdownTemporarilyHidden = true;
     onSelect(option);
   }
 
@@ -82,6 +86,7 @@
       on:focus={filterOptions}
       on:input={filterOptions}
       on:keydown={handleKeydown}
+      on:click={() => dropdownTemporarilyHidden = false}
     >
   </label>
   {#if dropdownShown}

commit feddfa60da6035a3cf436d584c0a076de2496617

Fix input dropdown style mismatch
Caused by ba15a95f

src/components/SearchBox.svelte
@@ -87,6 +87,7 @@
       on:input={filterOptions}
       on:keydown={handleKeydown}
       on:click={() => dropdownTemporarilyHidden = false}
+      class:dropdownShown
     >
   </label>
   {#if dropdownShown}
@@ -141,7 +142,7 @@
     border-bottom: thin solid transparent;
   }
 
-  input:focus {
+  input.dropdownShown {
     border-radius: 0.5em 0.5em 0 0;
     border-bottom: thin solid gray;
   }