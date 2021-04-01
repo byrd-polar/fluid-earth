ad70e1f5e02d631d6de971c9be3ca9c33b35ca40
Em Zhan <zqianem@gmail.com>
2021-04-01 11:11:43 -0400

    Ensure updated forecast data is downloaded
    Relevant for the times GFS data is overwritten with later-arriving GDAS
    data. Still allows for caching on the client-side, but forces a check
    with the server to see if file has been updated since.

--- src/fetcher.js
+++ src/fetcher.js
@@ -77,6 +77,9 @@ export default class Fetcher {
           this._resetIfComplete();
         },
         timeout: false,
+        headers: {
+          'Cache-Control': 'no-cache',
+        },
       });
       if (url.split('.').pop() === 'json') {
         data = await res.json();
