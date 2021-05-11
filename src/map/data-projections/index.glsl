0f5829d43c5e307986566a1b6a50183c2f801831
Em Zhan <zqianem@gmail.com>
2021-05-11 14:37:28 -0400

    Add shader code for OSCAR dataset
    Co-authored-by: Adelyn Arens <arens.28@buckeyemail.osu.edu>
    Co-authored-by: Michael Gravina <gravina.2@osu.edu>

--- src/map/data-projections/index.glsl
+++ src/map/data-projections/index.glsl
@@ -27,4 +27,21 @@ void projectToTexture(
     textureCoord = (lonLat + vec2(0, PI_2)) / vec2(2.0 * PI, PI);
     textureCoord.x = mod(textureCoord.x, 1.0);
   }
+  // OSCAR
+  else if (projection == 2) {
+
+    textureCoord = (lonLat + vec2(radians(-20.0), PI_2)) / vec2(2.0 * PI, PI);
+
+    float xOffset = 0.5 / gridWidth;
+    float xScale = 1080.0 / gridWidth;
+    float yScale = (90.0 / 80.0) * (gridHeight - 1.0) / gridHeight;
+
+    textureCoord.x = mod(xScale * (textureCoord.x + xOffset), xScale);
+    textureCoord.y = 1.0 - (yScale * (textureCoord.y - 0.5) + 0.5);
+
+    // get a NaN (actually -Infinity) value for areas outside of texture
+    if (textureCoord.y > 1.0 || textureCoord.y < 0.0) {
+      textureCoord = vec2(0.0, 0.5);
+    }
+  }
 }
