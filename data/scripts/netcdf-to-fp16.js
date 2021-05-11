533b3fc7d3697ab78b8e7691f5f85d6655be8cae
Em Zhan <zqianem@gmail.com>
2021-05-11 14:12:39 -0400

    Add script to convert NetCDF to fp16
    For use in future data pipelines (OSCAR and GEOS)
    
    Co-authored-by: Adelyn Arens <arens.28@buckeyemail.osu.edu>
    Co-authored-by: Michael Gravina <gravina.2@osu.edu>

---
+++ data/scripts/netcdf-to-fp16.js
@@ -0,0 +1,49 @@
+#!/bin/env node
+// Convert a NETCDF file to fp16
+//
+// Usage: node netcdf-to-fp16.js <inputFile> <outputFile> <variable>
+
+import { Float16Array } from '@petamoriken/float16';
+import { spawn } from 'child_process';
+import { Transform } from 'stream';
+import { createWriteStream } from 'fs';
+import { platform } from 'os';
+import split2 from 'split2';
+
+if (process.argv.length != 2 + 3) {
+  console.log('wrong number of arguments, expected 3\n');
+  console.log(
+    'Usage: node netcdf-to-fp16.js <inputFile> <outputFile> <variable>'
+  );
+  process.exit(1);
+}
+
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
+const variable = process.argv[4];
+
+const netcdf = spawn('ncdump', ['-v', variable, inputFile]);
+
+let inHeader = true;
+
+const float16 = new Transform({
+  transform(chunk, _encoding, callback) {
+    const chunkAsString = chunk.toString();
+
+    if (inHeader) {
+      inHeader = chunkAsString !== ` ${variable} =`;
+      callback(null, Buffer.alloc(0));
+      return;
+    }
+
+    const chunkAsFloats =
+      chunkAsString.split(/[,;]/).slice(0, -1).map(x => parseFloat(x));
+    const original = new Float32Array(chunkAsFloats);
+    const converted = new Float16Array(original);
+    callback(null, Buffer.from(converted.buffer));
+  }
+ });
+
+const output = createWriteStream(outputFile);
+
+netcdf.stdout.pipe(split2()).pipe(float16).pipe(output);
