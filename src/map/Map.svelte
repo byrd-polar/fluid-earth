byrd-polar/fluid-earth

commit 993de97433da3bfc42b9beb0ae9e6ec02cf20f22

Add support for arbitrary data projections
Creates the infrastructure needed to support gridded data sources in any
projection, not just lon-lat with/without offsets. Data projections
still need to implemented manually for each dataset however, as trying
to specify the parameters for any projection would get quite messy.

Additionally, modifies the griddedData and particleData objects to have
a `get` function to retrieve latLon data directly based on the data
projection instead of having to import a helper method.

src/map/Map.svelte
@@ -23,7 +23,6 @@
   export let centerLatitude = 0; // in degrees
   export let zoom = 1;
   export let projection = projections.VERTICAL_PERSPECTIVE;
-  export let offsetGriddedProjection = true;
 
   export let griddedData = {
     floatArray: new Float16Array([0]),
@@ -87,7 +86,8 @@
     u_lat0: centerLatitude,
     u_zoom: zoom,
     u_projection: projection.id,
-    u_offsetGridded: offsetGriddedProjection,
+    u_griddedDataProjection: griddedData.projection.id,
+    u_particleDataProjection: particleData.projection.id,
   };
 
   let backgroundGl;

data/gfs.js
@@ -14,6 +14,7 @@ const gfs0p25props = {
   height: 721,
   intervalInHours: 1,
   forecastIntervalInHours: 6,
+  projection: 'GFS',
 };
 
 const simpleScript = path.join('data', 'scripts', 'gfs-to-fp16.js');

data/rtgssthr.js
@@ -19,7 +19,7 @@ const datasetBase = {
   width: 4320,
   height: 2160,
   intervalInHours: 24,
-  offsetGriddedProjection: false,
+  projection: 'RTGSSTHR',
 };
 
 const [inventory, writeAndUnlockInventory] = await util.lockAndReadInventory();

src/App.svelte
@@ -15,6 +15,10 @@
   import Map, { updateAllWebglResolutions } from './map/Map.svelte';
   import colormaps from './map/colormaps/';
   import projections from './map/projections/';
+  import dataProjections, {
+    singleArrayDataGet,
+    pairedArrayDataGet,
+  } from './map/data-projections/';
 
   import Pins from './overlays/Pins.svelte';
   import Legends from './overlays/Legends.svelte';
@@ -54,7 +58,6 @@
   let updateParticleData = () => {};
 
   let projection = projections.VERTICAL_PERSPECTIVE;
-  let offsetGriddedProjection = true;
   let d3geoProjection = projection.function;
   let centerLongitude = -60;
   let centerLatitude = 30;
@@ -65,6 +68,8 @@
     width: 1,
     height: 1,
     originalUnit: griddedDataset.originalUnit,
+    projection: dataProjections.GFS,
+    get: lonLat => NaN,
   };
   let griddedColormap = griddedDataset.colormap;
   let griddedDomain = griddedDataset.domain;
@@ -75,6 +80,8 @@
     vVelocities: new Float16Array([0]),
     width: 1,
     height: 1,
+    projection: dataProjections.GFS,
+    get: lonLat => NaN,
   };
   let particleData = emptyParticleData;
 
@@ -140,14 +147,14 @@
           width: griddedDataset.width,
           height: griddedDataset.height,
           originalUnit: griddedDataset.originalUnit,
-        }
+          projection: griddedDataset.projection,
+          get: lonLat => singleArrayDataGet(griddedData, lonLat),
+        };
 
         if (previousGriddedDataset !== griddedDataset) {
           griddedDomain = griddedDataset.domain;
           griddedColormap = griddedDataset.colormap;
           griddedUnit = griddedDataset.unit;
-          offsetGriddedProjection = griddedDataset.offsetGriddedProjection ||
-            griddedDataset.offsetGriddedProjection === undefined;
 
           previousGriddedDataset = griddedDataset;
         }
@@ -182,6 +189,8 @@
           vVelocities: vArray,
           width: particleDataset.width,
           height: particleDataset.height,
+          projection: particleDataset.projection,
+          get: lonLat => pairedArrayDataGet(particleData, lonLat),
         };
 
         if (previousParticleDataset !== particleDataset) {
@@ -320,7 +329,6 @@
 <main>
   <Map
     {projection}
-    {offsetGriddedProjection}
     {centerLongitude}
     {centerLatitude}
     {zoom}

src/components/LocationsList.svelte
@@ -3,7 +3,6 @@
   import PinIcon from 'carbon-icons-svelte/lib/LocationHeartFilled32';
   import Close32 from "carbon-icons-svelte/lib/Close32";
   import { slide } from 'svelte/transition';
-  import { dataPoint } from '../map/gridded.js';
   import { convert, prettyUnit, prettyLatLon } from '../utility.js';
 
   export let pins;
@@ -11,9 +10,7 @@
   export let griddedUnit;
   export let moveTo;
 
-  $: values = pins.map(pin => {
-    return dataPoint(griddedData, [pin.longitude, pin.latitude]);
-  });
+  $: values = pins.map(pin => griddedData.get([pin.longitude, pin.latitude]));
 </script>
 
 <ul>

src/components/Pin.svelte
@@ -2,7 +2,6 @@
   import { fly, fade } from 'svelte/transition';
   import PinIcon from 'carbon-icons-svelte/lib/LocationHeartFilled32';
   import tooltip from '../tooltip.js';
-  import { dataPoint } from '../map/gridded.js';
   import { clipped } from '../map/projections/';
   import { convert, prettyUnit, prettyLatLon } from '../utility.js';
 
@@ -18,7 +17,7 @@
 
   $: [x, y] = d3geoProjection(lonLat);
   $: clip = clipped(d3geoProjection, lonLat);
-  $: value = dataPoint(griddedData, lonLat);
+  $: value = griddedData.get(lonLat);
 </script>
 
 

src/main.js
@@ -1,6 +1,7 @@
 import ky from 'ky';
 import App from './App.svelte';
 import colormaps from './map/colormaps/';
+import dataProjections from './map/data-projections/';
 
 (async () => {
   let inventory = await ky('/data/inventory.json', {timeout: false}).json();
@@ -17,6 +18,10 @@ import colormaps from './map/colormaps/';
     // replace colormap strings with colormap objects
     if ('colormap' in dataset) {
       dataset.colormap = colormaps[dataset.colormap];
+    }
+    // replace projection strings with projection objects
+    if ('projection' in dataset) {
+      dataset.projection = dataProjections[dataset.projection];
     }
   }
 

src/map/data-projections/gfs.glsl
@@ -1,0 +1,16 @@
+#pragma glslify: export(gfs)
+
+const float PI = radians(180.0);
+const float PI_2 = radians(90.0);
+
+void gfs(out vec2 textureCoord, in vec2 lonLat,
+    in float gridWidth, in float gridHeight) {
+  textureCoord = (lonLat + vec2(0, PI_2)) / vec2(2.0 * PI, PI);
+  textureCoord.y = 1.0 - textureCoord.y;
+
+  float xOffset = 0.5 / gridWidth;
+  float yScale = (gridHeight - 1.0) / gridHeight;
+
+  textureCoord.x = mod(textureCoord.x + xOffset, 1.0);
+  textureCoord.y = yScale * (textureCoord.y - 0.5) + 0.5;
+}

commit ffba9e5c61dca70538a426062171782fe06be693

Fix particle trails resetting on dataset change
Bug introduced by 993de97 (thank you, `git bisect`) that caused
`projectionUniforms` to update unnecessarily whenever `griddedData`
changed, even if the projection id was the same.

This fix takes advantage of Svelte not passing on reactive updates on
assignment of a primitive value if that value does not change.

src/map/Map.svelte
@@ -80,13 +80,15 @@
   let particleCanvas;
 
   let projectionUniforms;
+  $: u_griddedDataProjection = griddedData.projection.id;
+  $: u_particleDataProjection = particleData.projection.id
   $: projectionUniforms = {
     u_lon0: centerLongitude,
     u_lat0: centerLatitude,
     u_zoom: zoom,
     u_projection: projection.id,
-    u_griddedDataProjection: griddedData.projection.id,
-    u_particleDataProjection: particleData.projection.id,
+    u_griddedDataProjection,
+    u_particleDataProjection,
   };
 
   let backgroundGl;