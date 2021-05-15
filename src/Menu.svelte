ae109aab921eb079ca1671bcf059016d2e898116
Em Zhan <zqianem@gmail.com>
2021-05-15 14:01:35 -0400

    Fix header not overlapping calendar

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -113,6 +113,7 @@
       0 2px  4px -1px rgba(0,0,0,.2),
       0 4px  5px  0   rgba(0,0,0,.14),
       0 1px 10px  0   rgba(0,0,0,.12);
+    z-index: 1;
   }
 
   header > :global(button:first-child) {
