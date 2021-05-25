5f268cbb0f71777244ec4f789b96aa87f6805acb
danielh00 <djh91800@gmail.com>
2021-05-25 12:56:56 -0400

    Works with assim and forecast data

--- data/geos.js
+++ data/geos.js
@@ -8,7 +8,6 @@ import fs from 'fs/promises';
 import DomParser from 'dom-parser';
 import { debug } from 'console';
 
-const forecastHours = 6;
 const script = path.join('data', 'scripts', 'netcdf-to-fp16.js');
 const execFile = promisify(_execFile);
 
@@ -23,10 +22,10 @@ const geosProps = {
     projection: 'GEOS',
 };
 
-const hoursForwardDefault = 30;
+const hoursForecastStart = -2;
+const hoursForwardDefault = 2;
 const hoursForward00 = 240;
 const hoursForward12 = 120;
-
 
 const geosVars = [
     {
@@ -76,35 +75,16 @@ const geosVars = [
 ]
 const [inventory, writeAndUnlockInventory] = await util.lockAndReadInventory();
 
-function contains(dom, selector, text) {
-    var elements = dom.getElementsByTagName(selector);
-    return [].filter.call(elements, function (element) {
-        return RegExp(text).test(element.textContent);
-    });
-}
-async function findDataFromIndex(dataset, year, month, day, hour, forecast, indexPageURL) {
-    var parser = new DomParser();
-    const indexPageFilename = await util.download(indexPageURL, true);
-
-    var html = await fs.readFile(indexPageFilename, 'utf8');
-
-    var dom = parser.parseFromString(html);
-    var matchingAnchor = contains(dom, "a", `${forecast}.${year}${month}${day}_${hour}00z.nc4`);
-
-    var urlName = matchingAnchor[0].attributes[0]['value'];
-
-    dataset.filenamePrefix = urlName.substring(0, urlName.indexOf("." + forecast));
-
-    return urlName;
-}
-
-
-
-
-async function downloadGEOSData(dataset, year, month, day, hour, datetime, hourShift) {
+async function downloadGEOSData(dataset, datetime, hourShift) {
 
     const forecast = "inst1_2d_hwl_Nx";
-    const baseURL = 'https://portal.nccs.nasa.gov/datashare/gmao/geos-fp/das/' +
+
+    const year = datetime.year;
+    const month = datetime.toFormat('LL');
+    const day = datetime.toFormat('dd');
+    const hour = datetime.toFormat('HH');
+
+    const baseURL = 'https://portal.nccs.nasa.gov/datashare/gmao/geos-fp/forecast/' +
         `Y${year}/M${month}/D${day}/H${hour}/`;
 
     const nxtDatetime = datetime.plus({ hours: hourShift });
@@ -113,10 +93,10 @@ async function downloadGEOSData(dataset, year, month, day, hour, datetime, hourS
     const nxt_day = nxtDatetime.toFormat('dd');
     const nxt_hour = nxtDatetime.toFormat('HH')
 
-    const forecastNameSyntax = `GEOS.fp.fcst.${forecast}.${year}${month}${day}_${hour} +
-        ${nxt_year}${nxt_month}${nxt_day}_${nxt_hour}00z.V01.nc4`;
+    const forecastNameSyntax = `GEOS.fp.fcst.${forecast}.${year}${month}${day}_${hour}+` +
+        `${nxt_year}${nxt_month}${nxt_day}_${nxt_hour}00.V01.nc4`;
 
-    const inputFile = await util.download(baseURL + forecastNameSyntax, true);
+    const inputFile = await util.download(baseURL + forecastNameSyntax);
 
     return inputFile;
 }
@@ -131,20 +111,24 @@ for (const geosVar of geosVars) {
     let datetime;
 
     if (dataset) {
-        datetime = DateTime.fromISO(dataset.end, { zone: 'utc' }).plus({ hour: forecastHours });
+        datetime = DateTime.fromISO(dataset.lastForecast, { zone: 'utc' }).plus({ hour: geosVar.datasetBase.forecastIntervalInHours });
     } else {
         const now = DateTime.utc();
-        datetime = DateTime.utc(now.year, now.month, now.day).minus({ days: 1 });
+        datetime = DateTime.utc(now.year, now.month, now.day).minus({ hours: 18 });
+        datetime.set()
         inventory.push(dataset = geosVar.datasetBase);
     }
 
-    const year = datetime.year;
-    const month = datetime.toFormat('LL');
-    const day = datetime.toFormat('dd');
-    const hour = datetime.toFormat('HH')
+    const hour = datetime.toFormat('HH');
 
-    for (let f = 0; f < forecastHours; f++) {
-        const inputFile = await downloadGEOSData(dataset, year, month, day, hour);
+    var forecastHours = hoursForwardDefault;
+    if (hour == "00") {
+        forecastHours = hoursForward00;
+    } else if (hour == "12") {
+        forecastHours = hoursForward12;
+    }
+    for (let f = hoursForecastStart; f <= forecastHours; f++) {
+        const inputFile = await downloadGEOSData(dataset, datetime, f);
         const filename = datetime.plus({ hours: f }).toISO() + '.fp16';
         const outputFile = util.join(outputPath, filename);
 
@@ -155,8 +139,17 @@ for (const geosVar of geosVars) {
     for (const prop in geosVar.datasetBase) dataset[prop] = geosVar.datasetBase[prop];
 
     dataset.path = util.browserPath(outputPath);
-    dataset.start = dataset.start ?? datetime;
-    dataset.end = datetime;
+    dataset.start = dataset.start ?? datetime.plus({ hours: hoursForecastStart });
+
+    if (dataset.end) {
+        dataset.end = DateTime.max(
+            datetime.plus({ hours: forecastHours }),
+            DateTime.fromISO(dataset.end, { zone: 'utc' }),
+        );
+    } else {
+        dataset.end = datetime.plus({ hours: forecastHours });
+    }
+    dataset.lastForecast = datetime;
 
 }
 
