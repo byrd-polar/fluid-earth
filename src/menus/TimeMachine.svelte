aa23ca351a3527cfefe411d3e477f100e47031d3
Em Zhan <zqianem@gmail.com>
2021-06-15 20:32:52 -0400

    Remove Time Stepper
    Will be replaced with datetime picker and Timelapse menu

--- src/menus/TimeMachine.svelte
+++ src/menus/TimeMachine.svelte
@@ -1,7 +1,6 @@
 <script>
   import TimeJumper from '../components/TimeJumper.svelte';
   import Calendar from '../components/Calendar.svelte';
-  import TimeStepper from '../components/TimeStepper.svelte';
   import { getValidDates } from '../utility.js';
 
   export let date;
@@ -20,13 +19,6 @@
 <h2>Calendar</h2>
 <Calendar
   {griddedDataset}
-  bind:date
-  {utc}
-/>
-
-<h2>Time Stepper</h2>
-<TimeStepper
-  {validDates}
   bind:date
   {utc}
 />
