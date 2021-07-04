f9caaa93e1f7ef9ec9ce5c10c61d8461e30b6dca
Em Zhan <zqianem@gmail.com>
2021-07-04 12:52:28 -0400

    Email instructions on what to do upon unit failure

--- data/units/fev2r-email.sh
+++ data/units/fev2r-email.sh
@@ -8,4 +8,15 @@ Content-Transfer-Encoding: 8bit
 Content-Type: text/plain; charset=UTF-8
 
 $(systemctl status --full "$1")
+
+Please ssh into $(hostname) and run the following commands:
+
+    cd /export/data/fev2r/repository
+    npm run data:$(echo "$1" | cut -d'.' -f1 | cut -d'-' -f2)
+
+To investigate the cause of the issue, run the following command:
+
+    journalctl -u "$1" $(systemctl show -p MainPID "$1" | sed 's/Main/_/g')
+
+and type a capital "G" to jump to the end of the page.
 MAIL
