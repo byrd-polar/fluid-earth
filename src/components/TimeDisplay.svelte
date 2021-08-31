30899a1d584e9f65033e7681d110035e21b2021a
Em Zhan <zqianem@gmail.com>
2021-08-31 15:00:51 -0400

    Refactor time display overlay into own component

---
+++ src/components/TimeDisplay.svelte
@@ -0,0 +1,83 @@
+<script>
+  import tooltip from '../tooltip.js';
+  import { fix24 } from '../utility.js';
+
+  export let date;
+  export let utc;
+
+  $: dateOptions = {
+    timeZone: utc ? 'UTC' : undefined,
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  $: timeOptions = {
+    timeZone: utc ? 'UTC' : undefined,
+    hour12: utc ? false : undefined,
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+  };
+
+  function handleKeydown(e) {
+    if (!(e.code === 'Space' || e.code === 'Enter')) return;
+
+    utc = !utc
+  }
+</script>
+
+<section
+  on:click={() => utc = !utc}
+  on:keydown={handleKeydown}
+  use:tooltip={{ content: 'Change timezone', placement: 'bottom'}}
+  tabindex="0"
+>
+  <h3 class="date">{date.toLocaleDateString(undefined, dateOptions)}</h3>
+  <h3 class="time">
+    {fix24(date.toLocaleTimeString(undefined, timeOptions))}
+  </h3>
+</section>
+
+<style>
+  section {
+    cursor: pointer;
+    border-radius: 4px;
+    -webkit-tap-highlight-color: transparent;
+    transition: background-color 0.25s ease 0s;
+  }
+
+  section:focus, section:hover {
+    background-color: var(--input-color);
+    outline: none;
+  }
+
+  section:focus:not(:focus-visible):not(:hover) {
+    background-color: inherit;
+  }
+
+  h3 {
+    text-align: right;
+    font-size: 1.2em;
+    padding: 0.25rem 0.75rem 0;
+    margin: 0;
+    pointer-events: auto;
+  }
+
+  h3.time {
+    padding-top: 0;
+    font-weight: normal;
+  }
+
+  @media (max-width: 36rem) {
+    h3 {
+      font-size: 1em;
+      padding: 0.5rem 0.75rem;
+    }
+
+    h3.date {
+      padding: 0.5rem 0.75rem 0;
+    }
+  }
+</style>
--- src/overlays/Legends.svelte
+++ src/overlays/Legends.svelte
@@ -1,9 +1,8 @@
 <script>
+  import TimeDisplay from '../components/TimeDisplay.svelte';
   import GriddedLegend from '../components/GriddedLegend.svelte';
   import StreamlinesLegend from '../components/StreamlinesLegend.svelte';
   import SimpleStreamLegend from '../components/SimpleStreamlinesLegend.svelte';
-  import tooltip from '../tooltip.js';
-  import { validUnit, fix24, simplifyDataset } from '../utility.js';
 
   export let date;
   export let utc;
@@ -23,43 +22,14 @@
   export let particlesPaused;
 
   export let simplifiedMode;
-
-  $: dateOptions = {
-    timeZone: utc ? 'UTC' : undefined,
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-
-  $: timeOptions = {
-    timeZone: utc ? 'UTC' : undefined,
-    hour12: utc ? false : undefined,
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-  };
-
-  function handleKeydown(e) {
-    if (!(e.code === 'Space' || e.code === 'Enter')) return;
-
-    utc = !utc
-  }
 </script>
 
 <div class="wrapper">
   <div class="top" >
-    <section
-      on:click={() => utc = !utc}
-      on:keydown={handleKeydown}
-      use:tooltip={{ content: 'Change timezone', placement: 'bottom'}}
-      tabindex="0"
-    >
-      <h3 class="date">{date.toLocaleDateString(undefined, dateOptions)}</h3>
-      <h3 class="time">
-        {fix24(date.toLocaleTimeString(undefined, timeOptions))}
-      </h3>
-    </section>
+    <TimeDisplay
+      {date}
+      bind:utc
+    />
   </div>
   <div class="bottom">
     <GriddedLegend
@@ -116,35 +86,6 @@
     align-items: flex-end;
   }
 
-  section {
-    cursor: pointer;
-    border-radius: 4px;
-    -webkit-tap-highlight-color: transparent;
-    transition: background-color 0.25s ease 0s;
-  }
-
-  section:focus, section:hover {
-    background-color: var(--input-color);
-    outline: none;
-  }
-
-  section:focus:not(:focus-visible):not(:hover) {
-    background-color: inherit;
-  }
-
-  h3 {
-    text-align: right;
-    font-size: 1.2em;
-    padding: 0.25rem 0.75rem 0;
-    margin: 0;
-    pointer-events: auto;
-  }
-
-  h3.time {
-    padding-top: 0;
-    font-weight: normal;
-  }
-
   div.bottom {
     margin-top: auto;
     flex-wrap: wrap-reverse;
@@ -153,15 +94,6 @@
   @media (max-width: 36rem) {
     div.bottom :global(h3) {
       font-size: 0.875em;
-    }
-
-    h3 {
-      font-size: 1em;
-      padding: 0.5rem 0.75rem;
-    }
-
-    h3.date {
-      padding: 0.5rem 0.75rem 0;
     }
   }
 </style>
