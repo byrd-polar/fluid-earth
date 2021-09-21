acdeba987d91e4e9fb216d6ace5c93a565184691
Em Zhan <zqianem@gmail.com>
2021-09-21 14:29:16 -0400

    Disable context menu in kiosk mode
    Not perfect, as context menu can still be accessed when page is
    loading/reloading, but not worth the effort to strictly guard against
    this.

--- src/Kiosk.svelte
+++ src/Kiosk.svelte
@@ -9,9 +9,11 @@
   $: if (kioskMode) {
     restartTimer();
     resetEvents.forEach(e => document.addEventListener(e, restartTimer));
+    document.addEventListener('contextmenu', disableContextMenu);
   } else {
     stopTimer();
     resetEvents.forEach(e => document.removeEventListener(e, restartTimer));
+    document.removeEventListener('contextmenu', disableContextMenu);
   }
 
   function restartTimer() {
@@ -26,5 +28,9 @@
   function reloadPage() {
     window.history.replaceState(null, '', '#kmode=true');
     window.location.reload();
+  }
+
+  function disableContextMenu(e) {
+    e.preventDefault();
   }
 </script>
