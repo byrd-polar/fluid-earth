5bfc255e76fa20aaf867ea49455a6fa95dd56eef
Em Zhan <zqianem@gmail.com>
2021-12-17 16:58:40 -0600

    Fix trailing whitespace

--- src/Hash.svelte
+++ src/Hash.svelte
@@ -118,10 +118,10 @@
       let keys = Object.keys(pin);
       let expectedKeys = ['label', 'longitude', 'latitude'];
 
-      return keys.length === expectedKeys.length 
+      return keys.length === expectedKeys.length
         && expectedKeys.every(key => keys.includes(key))
         && typeof pin.label === 'string'
-        && typeof pin.latitude === 'number' 
+        && typeof pin.latitude === 'number'
         && typeof pin.longitude === 'number';
     })) {
       pins = val.map(p => {
