byrd-polar/fluid-earth

commit 2cc03b0c513f5d9e1246977e3a4214348e9ddbbb

Allow month to be changed during timelapse
Also fixes regression that prevented date changes in calendar from
pausing timelapse

src/components/calendar/Calendar.svelte
@@ -29,11 +29,15 @@
 
   $: picker = pickers[pickerMode];
 
-  $: headerDate = picker.headerDate(date);
+  $: headerDate = getHeaderDate(picker);
   $: prevHeaderDate = picker.prevHeaderDate(headerDate);
   $: nextHeaderDate = picker.nextHeaderDate(headerDate);
 
   $: boxDates = picker.boxDates(headerDate);
+
+  function getHeaderDate(picker) {
+    return picker.headerDate(date);
+  }
 
   function selectDate(boxDate) {
     if (picker.boxDateSelected(boxDate, date)) return;
@@ -53,7 +57,7 @@
     );
   }
 
-  function getInfo(boxDate) {
+  function getInfo(boxDate, headerDate, date, griddedDataset) {
     let wouldBeSelectedDate = selectedDate(boxDate, griddedDataset);
     return {
       boxDate,
@@ -96,7 +100,9 @@
       <RightArrow />
     </button>
   </div>
-  {#each boxDates.map(getInfo) as { boxDate, selected, enabled }, i (pickerMode + i)}
+  {#each boxDates.map(boxDate => {
+    return getInfo(boxDate, headerDate, date, griddedDataset);
+  }) as { boxDate, selected, enabled }, i (pickerMode + i)}
     <button
       class="box"
       class:hour={pickerMode === 'hours'}

commit 22cc198d4113ffdf62551ec2b3c9a089f1b44567

Partially revert 2cc03b0c
Caused unwanted behavior when changing date from other places (like the
hash or random date button)

Bug fix in original commit still present

src/components/calendar/Calendar.svelte
@@ -29,15 +29,11 @@
 
   $: picker = pickers[pickerMode];
 
-  $: headerDate = getHeaderDate(picker);
+  $: headerDate = picker.headerDate(date);
   $: prevHeaderDate = picker.prevHeaderDate(headerDate);
   $: nextHeaderDate = picker.nextHeaderDate(headerDate);
 
   $: boxDates = picker.boxDates(headerDate);
-
-  function getHeaderDate(picker) {
-    return picker.headerDate(date);
-  }
 
   function selectDate(boxDate) {
     if (picker.boxDateSelected(boxDate, date)) return;