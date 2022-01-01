9de5ba9d86030e48992780c701a7ea2b4dc7c388
Em Zhan <zqianem@gmail.com>
2022-01-01 14:33:25 -0600

    Fix bugs and re-arrange

--- src/components/calendar/miniTemporal.js
+++ src/components/calendar/miniTemporal.js
@@ -6,6 +6,14 @@ export class ZonedDateTime {
     this._utc = utc;
   }
 
+  get date() {
+    return new Date(this._date);
+  }
+
+  get daysInMonth() {
+    return this._daysInMonth(this.year, this.month);
+  }
+
   get year()        { return this._get('FullYear'); }
   get month()       { return this._get('Month') + 1; }
   get day()         { return this._get('Date'); }
@@ -13,10 +21,6 @@ export class ZonedDateTime {
   get minute()      { return this._get('Minutes'); }
   get second()      { return this._get('Seconds'); }
   get millisecond() { return this._get('Milliseconds'); }
-
-  _get(unit) {
-    return date[`get${this._utc ? 'UTC' : ''}${unit}`]();
-  }
 
   equals(other) {
     return this._date.getTime() === other._date.getTime();
@@ -33,12 +37,10 @@ export class ZonedDateTime {
       millisecond=this.millisecond,
     } = dateTimeLike;
 
-    let lastDayInMonth = this._fromDateArgs(year, month + 1, 0).day;
-
     return this._fromDateArgs(
       year,
       clamp(month, 1, 12) - 1,
-      clamp(day, 1, lastDayInMonth),
+      clamp(day, 1, this._daysInMonth(year, month)),
       clamp(hour, 0, 23),
       clamp(minute, 0, 59),
       clamp(second, 0, 59),
@@ -80,6 +82,14 @@ export class ZonedDateTime {
       Object.entries(durationLike).map(([key, val]) => [key, -val]));
 
     return this.add(negativeDurationLike);
+  }
+
+  _get(unit) {
+    return this._date[`get${this._utc ? 'UTC' : ''}${unit}`]();
+  }
+
+  _daysInMonth(year, month) {
+    return this._fromDateArgs(year, month, 0).day;
   }
 
   _fromDateArgs(...args) {
