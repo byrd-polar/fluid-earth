43402a112b2e086440be50f6fa083c4e58004583
Em Zhan <zqianem@gmail.com>
2022-01-26 22:57:17 -0600

    Add conversion functions for NetCDF and refactor

--- backend/file-conversions.js
+++ backend/file-conversions.js
@@ -36,6 +36,26 @@ export async function grib2_acc(inputs, output, options) {
   }, options.compression_level);
 }
 
+export async function netcdf(input, output, options) {
+  options = { ...defaults, ...options};
+  await writeFile(output, await array_to_data(
+    await netcdf_to_arr(input, options.variable),
+    v => isNaN(v) ? -Infinity : v * options.factor,
+    options.compression_level,
+  ));
+}
+
+export async function netcdf_speed(input, output, options) {
+  options = { ...defaults, ...options};
+  let [arrA, arrB] = await Promise.all(options.variables.map(v => {
+    return netcdf_to_arr(input, v);
+  }));
+  await writeFile(output, await array_to_data(arrA, (a, i) => {
+    let v = Math.hypot(a, arrB[i]);
+    return isNaN(v) ? -Infinity : v * options.factor;
+  }, options.compression_level));
+}
+
 async function gfs_combine_grib(inputs, output, combine_fn, compression_level) {
   let [arrA, arrB] = await Promise.all(inputs.map(grib2_to_arr));
 
@@ -55,14 +75,45 @@ async function grib2_to_arr(input) {
       s.on('open', () => resolve(s));
     });
   }
-  return new Promise((resolve, reject) => {
-    let child = spawn('wgrib2', [
+  return await spawn_cmd(
+    'wgrib2',
+    [
       '-',
       '-inv', platform() === 'win32' ? 'NUL' : '/dev/null',
       '-bin', '-',
       '-no_header',
       '-order', 'we:sn'
-    ], { stdio: [input, 'pipe', 'pipe'] });
+    ],
+    { stdio: [input, 'pipe', 'pipe'] },
+    [input],
+    (buf, resolve) => {
+      resolve(new Float32Array(buf.buffer, buf.byteOffset, buf.length / 4));
+    },
+  );
+}
+
+async function netcdf_to_arr(input, variable) {
+  return await spawn_cmd(
+    'ncdump',
+    ['-v', variable, '-p', '9,17', input],
+    {},
+    [],
+    (buffer, resolve) => {
+      resolve(
+        buffer
+        .toString()
+        .split(` ${variable} =`)[1]
+        .split(/[,;]/)
+        .slice(0, -1)
+        .map(x => parseFloat(x))
+      );
+    },
+  );
+}
+
+async function spawn_cmd(command, args, options, rejects, process_data_fn) {
+  return new Promise((resolve, reject) => {
+    let child = spawn(command, args, options);
 
     let { stdout, stderr } = child;
     let chunks = [];
@@ -73,15 +124,14 @@ async function grib2_to_arr(input) {
 
     child.on('close', code => {
       if (code === 0) {
-        let buf = Buffer.concat(chunks);
-        resolve(new Float32Array(buf.buffer, buf.byteOffset, buf.length / 4));
+        process_data_fn(Buffer.concat(chunks), resolve);
       } else {
         let msg = Buffer.concat(errs).toString();
-        reject(`wgrib2 exited with code ${code}:\n${msg}`);
+        reject(`${command} exited with code ${code}:\n${msg}`);
       };
     });
 
-    for (let obj of [child, input, stdout, stderr]) {
+    for (let obj of [child, stdout, stderr, ...rejects]) {
       obj.on('error', reject);
     }
   });
