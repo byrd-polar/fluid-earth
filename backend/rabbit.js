355cd3f667af0e32cec649a3a51b5a1fe75a2e7b
Em Zhan <zqianem@gmail.com>
2022-02-21 18:36:30 -0600

    Reorder so that simpler things come first

--- backend/rabbit.js
+++ backend/rabbit.js
@@ -18,13 +18,16 @@ let source_path = process.argv[2];
 let source = basename(source_path, '.js');
 
 let {
-  forage,
   minutes_of_sleep,
   minutes_of_sleep_if_failure,
   minutes_of_sleep_if_success,
+  forage,
 } = await import(source_path);
 
 parentPort?.postMessage(minutes_of_sleep_if_failure ?? minutes_of_sleep);
+
+let state_file = join(state_dir, `${source}.json`);
+let current_state = read_json(state_file, {});
 
 let dataset_files = await readdir(datasets_dir);
 let datasets = (await Promise.all(dataset_files.map(async file => {
@@ -37,10 +40,9 @@ let datasets = (await Promise.all(dataset_files.map(async file => {
   return { output_dir, ...dataset };
 }))).filter(d => d !== undefined);
 
-let state_file = join(state_dir, `${source}.json`);
-let current_state = read_json(state_file, {});
+let { new_state={}, metadatas=[] } = await forage(current_state, datasets);
 
-let { metadatas=[], new_state={} } = await forage(datasets, current_state);
+await write_json_atomically(state_file, new_state);
 
 for (let [index, metadata] of metadatas.entries()) {
   let dataset = datasets[index];
@@ -52,7 +54,5 @@ for (let [index, metadata] of metadatas.entries()) {
   let metadata_file = join(output_dir, 'metadata.json');
   await write_json_atomically(metadata_file, metadata);
 }
-
-await write_json_atomically(state_file, new_state);
 
 parentPort?.postMessage(minutes_of_sleep_if_success ?? minutes_of_sleep);
