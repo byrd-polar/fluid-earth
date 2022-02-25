de039b0147b4f2c6edb900c79571dc46da6b4496
Em Zhan <zqianem@gmail.com>
2022-02-24 18:42:11 -0600

    Add convenience script for file conversions
    Mostly for ad-hoc testing

---
+++ backend/convert.js
@@ -0,0 +1,12 @@
+#!/bin/env node
+
+// Convert stuff to fp16.br. For example:
+//
+// ./convert.js grib2 ./input.grib2 ./output.fp16.br "{ factor: 1e4 }"
+//
+// (assuming input.grib2 is a file in the current directory)
+
+import * as conversions from './file-conversions.js';
+
+let [, , fn, input, output, options='{}'] = process.argv;
+await conversions[fn](input, output, (new Function(`return ${options}`))());
