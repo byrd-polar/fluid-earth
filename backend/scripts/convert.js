032745b9eafa0aca17b369538d366cc73c7e96b6
Em Zhan <zqianem@gmail.com>
2022-03-29 21:44:03 -0500

    Create separate directory for scripts

--- backend/scripts/convert.js
+++ backend/scripts/convert.js
@@ -6,7 +6,7 @@
 //
 // (assuming input.grib2 is a file in the current directory)
 
-import * as conversions from './file-conversions.js';
+import * as conversions from '../file-conversions.js';
 
 let [, , fn, input, output, options='{}'] = process.argv;
 await conversions[fn](input, output, (new Function(`return ${options}`))());
