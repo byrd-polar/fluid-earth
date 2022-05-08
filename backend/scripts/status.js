0cf7837bf18bb5733b42d3a34a0f7856c2b79459
Em Zhan <zqianem@gmail.com>
2022-05-08 18:58:43 -0500

    Add script to check status of Tera backend

---
+++ backend/scripts/status.js
@@ -0,0 +1,62 @@
+#!/bin/env -S deno run --allow-net
+
+const tera_url = 'https://fluid-earth.byrd.osu.edu/tera/';
+
+const minutes = 60 * 1000;
+const hours = 60 * minutes;
+const days = 24 * hours;
+
+class Up {
+  now = new Date();
+  down = false;
+
+  assert(obj, prop, max_delay, message) {
+    if (this.now - new Date(obj[prop]) > max_delay) {
+      console.log(`\n${message}.`);
+      console.log(obj);
+      this.down = true;
+    }
+  }
+}
+
+async function get(filename) {
+  let response = await fetch(tera_url + filename);
+  return response.json();
+}
+
+const up = new Up();
+
+console.log(`Checking status of ${tera_url}...`);
+console.log({ now: up.now.toISOString() });
+
+let heart = await get('heart.json');
+let state = await get('state.json');
+
+up.assert(
+  heart, 'last_beat',
+  5 * minutes, 'Tera is down',
+);
+up.assert(
+  state.sources.geos, 'forecast',
+  18 * hours, 'GEOS is delayed',
+);
+up.assert(
+  state.sources.gfs, 'forecast',
+  11 * hours, 'GFS is delayed',
+);
+up.assert(
+  state.sources.gfswave, 'forecast',
+  11 * hours, 'GFS-wave is delayed',
+);
+up.assert(
+  state.sources.oscar, 'date',
+  8 * days, 'OSCAR is delayed',
+);
+up.assert(
+  state.sources.rtgssthr, 'date',
+  47 * hours, 'RTGSSTHR is delayed',
+);
+
+if (!up.down) console.log('\nAll sources up to date.');
+
+Deno.exit(up.down ? 1 : 0);
