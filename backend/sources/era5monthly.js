91ea17f2e613bc3b309ef4f7912c3c31235026f0
Em Zhan <zqianem@gmail.com>
2022-05-27 13:43:22 -0400

    Sketch out era5monthly source

--- backend/sources/era5monthly.js
+++ backend/sources/era5monthly.js
@@ -1,6 +1,44 @@
-import 'dotenv/config' 
+import { Datetime } from '../datetime.js';
 import { download, get_json, post_json } from '../download.js'
+import { grib1 } from '../file-conversions.js';
+import { typical_metadata, output_path, run_all } from '../utility.js';
 import { setTimeout as sleep } from 'timers/promises';
+import 'dotenv/config'
+
+const shared_metadata = {
+};
+
+export async function forage(current_state, datasets) {
+  let { date, source_last_updated } = current_state;
+  let dt = date
+    ? Datetime.from(date).add({ months: 1 })
+    : Datetime.from('1979-01-01');
+  date = dt.to_iso_string();
+
+  // check
+  // https://cds.climate.copernicus.eu/api/v2.ui/resources/reanalysis-era5-single-levels-monthly-means
+  // to see if updated_date has changed
+  if (true) throw 'No update needed';
+
+  let metadatas = datasets.map(d => typical_metadata(d, dt, shared_metadata));
+
+  let input = await download_cds('reanalysis-era5-single-levels', {
+    product_type: 'reanalysis',
+    format: 'grib',
+    variable: '2m_temperature',
+    year: dt.year,
+    month: dt.p_month,
+    time: '00:00',
+    day: '01',
+  }, process.env.CDS_API_KEY);
+
+  await run_all(datasets.map(dataset => async () => {
+
+  }));
+  await rm(input);
+
+  return { metadatas, new_state: { date, source_last_updated } };
+}
 
 async function download_cds(dataset_name, request, auth) {
   let base_url = 'https://cds.climate.copernicus.eu/api/v2';
@@ -26,13 +64,3 @@ async function download_cds(dataset_name, request, auth) {
 
   return download(download_url, { auth });
 }
-
-await download_cds('reanalysis-era5-single-levels', {
-  product_type: 'reanalysis',
-  format: 'grib',
-  variable: '2m_temperature',
-  year: '2021',
-  month: '08',
-  time: '00:00',
-  day: '01',
-}, process.env.CDS_API_KEY);
