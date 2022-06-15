b3c97c6a83c20033e841e37c535fa5cb25ae495d
Em Zhan <zqianem@gmail.com>
2022-06-15 11:44:14 -0500

    Fix use of top-level await preventing build

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import App from './App.svelte';
 import { GriddedDataset, ParticleDataset } from './datasets.js';
 import { fetchJson } from './utility.js';
 
-let inventory = await fetchJson('/tera/inventory.json.br', {
-  headers: { 'Cache-Control': 'no-cache' },
-});
+(async () => {
+  let inventory = await fetchJson('/tera/inventory.json.br', {
+    headers: { 'Cache-Control': 'no-cache' },
+  });
 
-let gDatasets = [...GriddedDataset.filter(inventory), GriddedDataset.none];
-let pDatasets = [...ParticleDataset.filter(inventory), ParticleDataset.none];
+  let gDatasets = [...GriddedDataset.filter(inventory), GriddedDataset.none];
+  let pDatasets = [...ParticleDataset.filter(inventory), ParticleDataset.none];
 
-new App({ target: document.body, props: { gDatasets, pDatasets } });
+  new App({ target: document.body, props: { gDatasets, pDatasets } });
+})();
