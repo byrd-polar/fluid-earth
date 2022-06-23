byrd-polar/fluid-earth

commit 354823afd149dcee2c419220b688ce694dcf2a96

Use constructor instead of getters; refactor
Avoids recalculating values unnecessarily and allows us to remove a
cache for OSCAR dates

Downside is that assignments in constructor are order-dependent, so be
careful when modifying/adding props

Adds new prop to Dataset objects: `validDates`

src/datasets.js
@@ -9,32 +9,33 @@ import { ZonedDateTime } from './temporal.js'
 import { findClosestDateInAscendingList } from './utility.js'
 import { Float16Array } from '@petamoriken/float16'
 
+const now = new Date()
 const zeroProxy = new Proxy({}, { get() { return 0 } })
 const nanDataArray = new Float16Array([-Infinity])
 const zeroDataArray = new Float16Array([0])
 
 class Dataset {
-  constructor(core)  { this.core = core ?? {} }
-  get name()         { return this.core.name ?? 'none' }
-  get path()         { return this.core.path ?? null }
-  get width()        { return this.core.width ?? 1 }
-  get height()       { return this.core.height ?? 1 }
-  get start()        { return new Date(this.core.start ?? new Date()) }
-  get end()          { return new Date(this.core.end ?? new Date()) }
-  get missing()      { return (this.core.missing ?? []).map(d => new Date(d)) }
-  get interval()     {
-    switch(this.core.intervalInHours) {
-      case 1: return intervals.hourly
-      case 24: return intervals.daily
-      case 'custom:OSCAR': return intervals['custom:OSCAR']
-      default: return intervals[this.core.interval] ?? intervals['custom:NONE']
-    }
-  }
-  get projection()   { return dataProjections[this.core.projection ?? 'GFS'] }
-  get bytesPerFile() { return this.width * this.height * 2 }
-
-  get dataProps() {
-    return {
+  constructor(core={}) {
+    this.core         = core
+    this.name         = core.name ?? 'none'
+    this.path         = core.path ?? null
+    this.width        = core.width ?? 1
+    this.height       = core.height ?? 1
+    this.start        = new Date(core.start ?? now)
+    this.end          = new Date(core.end ?? now)
+    this.interval     = (() => {
+      switch(core.intervalInHours) {
+        case 1: return intervals.hourly
+        case 24: return intervals.daily
+        case 'custom:OSCAR': return intervals['custom:OSCAR']
+        default: return intervals[core.interval] ?? intervals['custom:NONE']
+      }
+    })()
+    this.missing      = (core.missing ?? []).map(d => new Date(d))
+    this.validDates   = this.computeValidDates()
+    this.projection   = dataProjections[core.projection ?? 'GFS']
+    this.bytesPerFile = this.width * this.height * 2
+    this.dataProps    = {
       width: this.width,
       height: this.height,
       projection: this.projection,
@@ -120,7 +121,7 @@ class Dataset {
           excludedDate=undefined,
         } = oscarOptions
 
-        let candidates = this.computeValidDates().filter(c => {
+        let candidates = this.validDates.filter(c => {
           return (!preserveMonth || c.getMonth() === date.getMonth())
               && (!preserveUTCMonth || c.getUTCMonth() === date.getUTCMonth())
               && (!excludedDate || c.getTime() !== excludedDate.getTime())
@@ -133,7 +134,7 @@ class Dataset {
           .date
 
         return this.isMissingDate(closest)
-          ? findClosestDateInAscendingList(date, this.computeValidDates())
+          ? findClosestDateInAscendingList(date, this.validDates)
           : closest
     }
   }
@@ -170,19 +171,19 @@ export function addDatasetFetchListener(f) {
 export class GriddedDataset extends Dataset {
   static none = new GriddedDataset()
 
-  get type()         { return 'gridded' }
-  get colormap()     { return colormaps[this.core.colormap ?? 'VIRIDIS'] }
-  get domain()       { return this.core.domain ?? [0, 1] }
-  get unit()         { return this.core.unit ?? '' }
-  get originalUnit() { return this.core.originalUnit ?? '' }
-  get scale()        { return this.core.scale ?? 'linear' }
-
-  get dataProps() {
-    return {
+  constructor(core={}) {
+    super(core)
+    this.type         = 'gridded'
+    this.colormap     = colormaps[core.colormap ?? 'VIRIDIS']
+    this.domain       = core.domain ?? [0, 1]
+    this.unit         = core.unit ?? ''
+    this.originalUnit = core.originalUnit ?? ''
+    this.scale        = core.scale ?? 'linear'
+    this.dataProps    = {
       unit: this.unit,
       originalUnit: this.originalUnit,
       get(lonLat) { return singleArrayDataGet(this, lonLat) },
-      ...super.dataProps,
+      ...this.dataProps,
     }
   }
 
@@ -208,16 +209,16 @@ export class GriddedDataset extends Dataset {
 export class ParticleDataset extends Dataset {
   static none = new ParticleDataset()
 
-  get type()             { return 'particle' }
-  get particleLifetime() { return this.core.particleLifetime ?? 0 }
-  get particleCount()    { return this.core.particleCount ?? 0 }
-  get particleDisplay()  { return this.core.particleDisplay ?? zeroProxy }
-  get bytesPerFile()     { return 2 * super.bytesPerFile }
-
-  get dataProps() {
-    return {
+  constructor(core={}) {
+    super(core)
+    this.type             = 'particle'
+    this.particleLifetime = core.particleLifetime ?? 0
+    this.particleCount    = core.particleCount ?? 0
+    this.particleDisplay  = core.particleDisplay ?? zeroProxy
+    this.bytesPerFile     = 2 * this.bytesPerFile
+    this.dataProps        = {
       get(lonLat) { return pairedArrayDataGet(this, lonLat) },
-      ...super.dataProps,
+      ...this.dataProps,
     }
   }
 

src/components/RangeSettings.svelte
@@ -12,8 +12,7 @@
 
   $: date, updateStart();
 
-  $: datesAfterStart = griddedDataset.computeValidDates()
-    .filter(d => d >= start);
+  $: datesAfterStart = griddedDataset.validDates.filter(d => d >= start);
 
   $: validDates = datesAfterStart
     .filter((_, i) => i % (interval || 1) === 0)

src/menus/TimeMachine.svelte
@@ -11,7 +11,7 @@
   export let simplifiedMode;
 
   let playerActive, fps;
-  let validDates = griddedDataset.computeValidDates();
+  let validDates = griddedDataset.validDates; // reactive via RangeSettings
 </script>
 
 <details open>

src/oscar.js
@@ -10,27 +10,15 @@ export function nextOscarDate(currentDate) {
   return dates.find(d => d > currentDate);
 }
 
-let cache = new Map();
-
-function getKey(startDate, endDate) {
-  return `${startDate.getTime()}|${endDate.getTime()}`;
-}
-
 // Get all dates with OSCAR data between given start and end dates, inclusive
 export function validOscarDates(startDate, endDate) {
-  let key = getKey(startDate, endDate);
-  if (cache.has(key)) return cache.get(key);
-
   let startYear = startDate.getUTCFullYear();
   let endYear = endDate.getUTCFullYear();
   let dates = [];
   for (let year = startYear; year <= endYear; year++) {
     dates.push(...validOscarDatesForYear(year));
   }
-
-  dates = dates.filter(d => d >= startDate && d <= endDate);
-  cache.set(key, dates);
-  return dates;
+  return dates.filter(d => d >= startDate && d <= endDate);
 }
 
 // Given a year, return the Dates that correspond to the OSCAR data that

commit 07f64905818336587f45ee92e49cfcc45d188539

Fix major performance regression
Caused by 354823afd calculating all the valid dates for all datasets on
initial page load; now calculated when needed and cached

src/datasets.js
@@ -32,7 +32,6 @@ class Dataset {
       }
     })()
     this.missing      = (core.missing ?? []).map(d => new Date(d))
-    this.validDates   = this.computeValidDates()
     this.projection   = dataProjections[core.projection ?? 'GFS']
     this.bytesPerFile = this.width * this.height * 2
 
@@ -40,6 +39,8 @@ class Dataset {
     this.dataProps    = { width, height, projection }
     this.emptyData    = { width: 1, height: 1, projection }
   }
+
+  get validDates() { return this._validDates ??= this.computeValidDates() }
 
   fetchCache = {}
 