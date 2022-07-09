5e20214cdbcf145fc9abd8116cea9270763d1ce0
Em Zhan <zqianem@gmail.com>
2022-07-09 12:41:33 -0500

    Revert "Speed up initial render by preloading some JSON"
    This reverts commit 703b1d1ccff692a28e8db62e71b2d4281626e295.
    
    Doesn't work in Safari, see:
    
    https://bugs.webkit.org/show_bug.cgi?id=236009
    https://bugs.webkit.org/show_bug.cgi?id=233854

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { GriddedDataset, ParticleDataset } from './datasets.js';
 import { fetchJson } from './utility.js';
 
 (async () => {
-  let inventory = await fetchJson('/tera/inventory.json.br');
+  let inventory = await fetchJson('/tera/inventory.json.br', {
+    headers: { 'Cache-Control': 'no-cache' },
+  });
 
   let gDatasets = [...GriddedDataset.filter(inventory), GriddedDataset.none];
   let pDatasets = [...ParticleDataset.filter(inventory), ParticleDataset.none];
