c9515360442456867b834d1c0752a95661f51e58
Em Zhan <zqianem@gmail.com>
2022-10-05 18:05:57 -0500

    Add explanations for climate variables

--- src/menus/Datasets.svelte
+++ src/menus/Datasets.svelte
@@ -20,3 +20,11 @@
   {simplifiedMode}
 />
 </details>
+<details open>
+<summary><h2>Climate Information</h2></summary>
+<p>Temperature and precipitation averages (<b>avg.</b>) are over a monthly
+  period.
+<p>The <b>anomaly</b> is the difference between the average for a specific
+  month, e.g. August 2022, compared to an average over that same month from
+  1991-2020, e.g. all the months of August during 1991-2020.
+</details>
