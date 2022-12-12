1ffee2346b2abd3104edfaa036293321e478d30d
Michael Gravina <gravinamike@hotmail.com>
2022-12-12 11:57:48 -0500

    Added explanatory text for permafrost zones

--- src/menus/Datasets.svelte
+++ src/menus/Datasets.svelte
@@ -27,4 +27,12 @@
 <p>The <b>anomaly</b> is the difference between the average for a specific
   month, e.g. August 2022, compared to an average over that same month from
   1991-2020, e.g. all the months of August during 1991-2020.
+<p><b>Permafrost zones</b> are numbered from 1 to 4 based on how much of the
+  ground is covered by permafrost:
+  <ol>
+    <li>Isolated patches (0 - 10% coverage)</li>
+    <li>Sporadic permafrost (10 - 50% coverage)</li>
+    <li>Discontinuous permafrost (50 - 90% coverage)</li>
+    <li>Continuous permafrost (>90% coverage)</li>
+  </ol>
 </details>
