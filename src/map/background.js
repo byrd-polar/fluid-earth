bcf6ef955b71351c00f788beaef692ca37898574
Em Zhan <zqianem@gmail.com>
2023-04-07 16:12:05 -0500

    Use multiple data textures for higher resolutions
    If the max texture size for the webgl instance is larger than each
    dimension of the gridded data, render as normal. Otherwise, the gridded
    data is re-gridded into one or more textures with width equal to the max
    texture size and the `texelFetch` function is used. Data rendered this
    way cannot be linearly filtered to appear smooth.
    
    Removes the hack used to render sea surface temperature data on Android
    Chrome and the MAX_TEXTURE_SIZE variable previously exported from the
    Map component/webcomponent.

--- src/map/background.js
+++ src/map/background.js
@@ -22,6 +22,8 @@ export default class MapBackground {
     this._gl = gl;
     this._gl.enable(gl.BLEND);
     this._gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    this._maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+    this._dataTextureDimensions = this._getDataTextureDimensions();
 
     this._programs = this._createPrograms();
     this._buffers = this._createBuffers(options.vectorData);
@@ -35,12 +37,14 @@ export default class MapBackground {
   set data(d) {
     this._data = d;
 
-    this._gl.deleteTexture(this._textures.gridded);
-    this._gl.deleteTexture(this._textures.data);
+    this._textures.gridded.forEach(t => this._gl.deleteTexture(t));
+    this._textures.data.forEach(t => this._gl.deleteTexture(t));
     this._gl.deleteFramebuffer(this._framebuffers.gridded.framebuffer);
 
-    this._textures.gridded = this._createGriddedTexture();
-    this._textures.data = this._createDataTexture();
+    this._dataTextureDimensions = this._getDataTextureDimensions();
+
+    this._textures.gridded = this._createGriddedTextures();
+    this._textures.data = this._createDataTextures();
     this._framebuffers = this._createFramebuffers();
     this._dataNeedsRecolor = true;
   }
@@ -79,10 +83,15 @@ export default class MapBackground {
       this._mapDataToColors();
       this._dataNeedsRecolor = false;
     }
+    let textures = Object.fromEntries(
+      // our shader supports 16 textures, the webgl2 minimum
+      this._textures.gridded.map((t, i) => [`u_texture${i}`, t]).slice(0, 16)
+    );
     glDraw(this._gl, this._programs.gridded, this._buffers.gridded, {
-      u_texture: this._textures.gridded,
+      ...textures,
       u_gridWidth: this._data.width,
       u_gridHeight: this._data.height,
+      u_maxTextureSize: this._maxTextureSize,
       ...sharedUniforms,
     });
   }
@@ -127,35 +136,72 @@ export default class MapBackground {
 
   _createTextures() {
     return {
-      gridded: this._createGriddedTexture(),
-      data: this._createDataTexture(),
+      gridded: this._createGriddedTextures(),
+      data: this._createDataTextures(),
       colormap: this._createColormapTexture(),
     };
   }
 
-  _createGriddedTexture() {
-    return twgl.createTexture(this._gl, {
+  _createGriddedTextures() {
+    let options = {
       mag: this._gl.NEAREST, // show zoomed in data as individual pixels
       min: this._gl.LINEAR,
-      width: this._data.width,
-      height: this._data.height,
       auto: false,
+    };
+
+    return this._dataTextureDimensions.map(({ width, height }) => {
+      return twgl.createTexture(this._gl, { width, height, ...options });
     });
   }
 
-  _createDataTexture() {
-    let halfFloat = this._data.floatArray.constructor.name !== 'Float32Array';
-    let src = this._data.floatArray;
-
-    return twgl.createTexture(this._gl, {
-      src:            halfFloat ? new Uint16Array(src.buffer) : src,
+  _createDataTextures() {
+    let { floatArray } = this._data;
+    let halfFloat = floatArray.constructor.name !== 'Float32Array';
+    let options = {
       type:           halfFloat ? this._gl.HALF_FLOAT : this._gl.FLOAT,
       internalFormat: halfFloat ? this._gl.R16F : this._gl.R32F,
       format: this._gl.RED,
       minMag: this._gl.NEAREST, // don't filter between data points
-      width: this._data.width,
-      height: this._data.height,
+    };
+
+    let offset = 0;
+    floatArray = halfFloat ? new Uint16Array(floatArray.buffer): floatArray;
+
+    return this._dataTextureDimensions.map(({ width, height }) => {
+      let start = offset;
+      let end = start + (width * height);
+      offset = end;
+
+      let src;
+      if (end <= floatArray.length) {
+        src = floatArray.subarray(start, end);
+      } else {
+        src = new (halfFloat ? Uint16Array : Float32Array)(end - start);
+        src.set(floatArray.subarray(start, end));
+      }
+
+      return twgl.createTexture(this._gl, { src, width, height, ...options});
     });
+  }
+
+  _getDataTextureDimensions() {
+    let { width, height } = this._data;
+    if (width <= this._maxTextureSize && height <= this._maxTextureSize) {
+      return [{ width, height }];
+    }
+
+    let pixelCount = this._data.width * this._data.height;
+    let maxPixelsPerTexture = Math.pow(this._maxTextureSize, 2);
+    let length = Math.floor(pixelCount / maxPixelsPerTexture);
+
+    width = this._maxTextureSize;
+    height = this._maxTextureSize;
+    let lastHeight = Math.ceil((pixelCount % maxPixelsPerTexture) / width) + 1;
+
+    return [
+      ...Array(length).fill({ width, height }),
+      { width, height: lastHeight },
+    ];
   }
 
   _createColormapTexture() {
@@ -170,29 +216,33 @@ export default class MapBackground {
 
   _createFramebuffers() {
     return {
-      gridded: twgl.createFramebufferInfo(
-        this._gl,
-        [{ attachment: this._textures.gridded }],
-        this._data.width,
-        this._data.height,
-      ),
+      gridded: this._dataTextureDimensions.map(({ width, height}, i) => {
+        return twgl.createFramebufferInfo(
+          this._gl,
+          [{ attachment: this._textures.gridded[i] }],
+          width,
+          height,
+        );
+      }),
     };
   }
 
   // draw a texture that represents the (gridded) data using the selected
   // colormap, domain, and scale type
   _mapDataToColors() {
-    // switch rendering destination to our framebuffer
-    twgl.bindFramebufferInfo(this._gl, this._framebuffers.gridded);
+    for (let i = 0; i < this._textures.data.length; i++) {
+      // switch rendering destination to our framebuffer
+      twgl.bindFramebufferInfo(this._gl, this._framebuffers.gridded[i]);
 
-    glDraw(this._gl, this._programs.colormap, this._buffers.colormap, {
-      u_data: this._textures.data,
-      u_colormap: this._textures.colormap,
-      u_colormapN: this._colormap.lut.length,
-      u_domain: this._domain,
-      u_scale: this._scale === 'log' ? 1 : 0,
-      u_baseColor: this._baseColor.map((v, i) => i === 3 ? v : v / 255),
-    });
+      glDraw(this._gl, this._programs.colormap, this._buffers.colormap, {
+        u_data: this._textures.data[i],
+        u_colormap: this._textures.colormap,
+        u_colormapN: this._colormap.lut.length,
+        u_domain: this._domain,
+        u_scale: this._scale === 'log' ? 1 : 0,
+        u_baseColor: this._baseColor.map((v, i) => i === 3 ? v : v / 255),
+      });
+    }
 
     // switch rendering destination back to canvas
     twgl.bindFramebufferInfo(this._gl, null);
--- src/map/gridded.frag
+++ src/map/gridded.frag
@@ -2,6 +2,7 @@
 // Fragment shader for the gridded data layer
 
 precision highp float;
+precision highp int;
 
 #pragma glslify: invertProject = require(./projections/invert.glsl)
 #pragma glslify: projectToTexture = require(./data-projections/)
@@ -14,9 +15,26 @@ uniform int u_projection;
 uniform bool u_translateY;
 uniform int u_griddedDataProjection;
 
-uniform sampler2D u_texture;
+uniform sampler2D u_texture0;
+uniform sampler2D u_texture1;
+uniform sampler2D u_texture2;
+uniform sampler2D u_texture3;
+uniform sampler2D u_texture4;
+uniform sampler2D u_texture5;
+uniform sampler2D u_texture6;
+uniform sampler2D u_texture7;
+uniform sampler2D u_texture8;
+uniform sampler2D u_texture9;
+uniform sampler2D u_texture10;
+uniform sampler2D u_texture11;
+uniform sampler2D u_texture12;
+uniform sampler2D u_texture13;
+uniform sampler2D u_texture14;
+uniform sampler2D u_texture15;
+
 uniform float u_gridWidth;
 uniform float u_gridHeight;
+uniform float u_maxTextureSize;
 
 in vec2 v_position;
 out vec4 color;
@@ -69,5 +87,52 @@ void main() {
       u_griddedDataProjection
   );
 
-  color = texture(u_texture, textureCoord);
+  if (u_gridWidth <= u_maxTextureSize && u_gridHeight <= u_maxTextureSize) {
+    color = texture(u_texture0, textureCoord);
+    return;
+  }
+
+  int r = int(textureCoord.y * u_gridHeight);
+  int c = int(textureCoord.x * u_gridWidth);
+  int idx = r * int(u_gridWidth) + c;
+
+  int row = idx / int(u_maxTextureSize);
+  int col = int(mod(float(idx), u_maxTextureSize));
+  int texNum = row / int(u_maxTextureSize);
+
+  ivec2 coord = ivec2(col, int(mod(float(row), u_maxTextureSize)));
+
+  if (texNum == 0) {
+    color = texelFetch(u_texture0, coord, 0);
+  } else if (texNum == 1)  {
+    color = texelFetch(u_texture1, coord, 0);
+  } else if (texNum == 2)  {
+    color = texelFetch(u_texture2, coord, 0);
+  } else if (texNum == 3)  {
+    color = texelFetch(u_texture3, coord, 0);
+  } else if (texNum == 4)  {
+    color = texelFetch(u_texture4, coord, 0);
+  } else if (texNum == 5)  {
+    color = texelFetch(u_texture5, coord, 0);
+  } else if (texNum == 6)  {
+    color = texelFetch(u_texture6, coord, 0);
+  } else if (texNum == 7)  {
+    color = texelFetch(u_texture7, coord, 0);
+  } else if (texNum == 8)  {
+    color = texelFetch(u_texture8, coord, 0);
+  } else if (texNum == 9)  {
+    color = texelFetch(u_texture9, coord, 0);
+  } else if (texNum == 10) {
+    color = texelFetch(u_texture10, coord, 0);
+  } else if (texNum == 11) {
+    color = texelFetch(u_texture11, coord, 0);
+  } else if (texNum == 12) {
+    color = texelFetch(u_texture12, coord, 0);
+  } else if (texNum == 13) {
+    color = texelFetch(u_texture13, coord, 0);
+  } else if (texNum == 14) {
+    color = texelFetch(u_texture14, coord, 0);
+  } else if (texNum == 15) {
+    color = texelFetch(u_texture15, coord, 0);
+  }
 }
