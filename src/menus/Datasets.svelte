78eff1859891b1d8aa6aaf6d493af682fb56af57
Em Zhan <zqianem@gmail.com>
2023-04-10 13:45:47 -0500

    Update description of permafrost dataset

--- src/menus/Datasets.svelte
+++ src/menus/Datasets.svelte
@@ -27,13 +27,14 @@
 <p>The <b>anomaly</b> is the difference between the average for a specific
   month, e.g. August 2022, compared to an average over that same month from
   1991-2020, e.g. all the months of August during 1991-2020.
-<p><b>Permafrost zones</b> are shown for the northern hemisphere only, and
-  are numbered from 1 to 4 based on how much of the ground is covered by
-  permafrost:
-  <ol>
-    <li>Isolated patches (0 - 10% coverage)</li>
-    <li>Sporadic permafrost (10 - 50% coverage)</li>
-    <li>Discontinuous permafrost (50 - 90% coverage)</li>
-    <li>Continuous permafrost (>90% coverage)</li>
-  </ol>
+
+<p><b>Permafrost probability</b> is how likely that the average annual ground
+  temperature is below 0 °C. Based on this value, permafrost zones are
+  classified as follows:
+  <ul>
+    <li>Isolated patches (&gt;0–10%)</li>
+    <li>Sporadic permafrost (10–50%)</li>
+    <li>Discontinuous permafrost (50–90%)</li>
+    <li>Continuous permafrost (90%–100%)</li>
+  </ul>
 </details>
