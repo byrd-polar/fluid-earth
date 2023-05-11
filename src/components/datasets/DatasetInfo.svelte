3eaafa1ca4b3e111b494b0d8ec4c84d1c27b3a8c
Em Zhan <zqianem@gmail.com>
2023-05-11 08:45:54 -0500

    Display additional information per dataset
    Replaces the Climate Information section

---
+++ src/components/datasets/DatasetInfo.svelte
@@ -0,0 +1,61 @@
+<script>
+  export let griddedDataset;
+  $: info = infoByName(griddedDataset.name);
+
+  function infoByName(name) {
+    switch(name) {
+      case 'average temperature at 2 m above ground':
+      case 'average precipitation per day':
+        return 'climate_averages';
+      case 'average temperature at 2 m above ground anomaly':
+      case 'average precipitation per day anomaly':
+        return 'climate_anomolies';
+      case 'permafrost probability':
+        return 'permafrost_prob';
+      default:
+        return 'default';
+    }
+  }
+</script>
+
+{#if info === 'climate_averages'}
+  <p>Climate averages (<b>avg.</b>) are over a monthly period.</p>
+{:else if info === 'climate_anomolies'}
+  <p>The <b>anomaly</b> is the difference between the average for a specific
+    month, e.g. August 2022, compared to an average over that same month from
+    1991–2020, e.g. all the months of August during 1991–2020.</p>
+{:else if info === 'permafrost_prob'}
+  <p><b>Permafrost probability</b> is how likely that the average annual ground
+    temperature is below 0&nbsp;°C. Based on this value, permafrost zones are
+    classified as follows:
+  <dl>
+    <dt>&gt;0–10%<dd>isolated patches
+    <dt>&gt;10–50%<dd>sporadic
+    <dt>&gt;50–90%<dd>discontinuous
+    <dt>&gt;90–100%<dd>continuous
+  </dl>
+{:else if info === 'default'}
+  <p class="italics">No additional information for this dataset.</p>
+{/if}
+
+<style>
+  dl {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+  }
+
+  dt {
+    font-weight: bold;
+    text-align: right;
+    grid-column: 1;
+  }
+
+  dd {
+    grid-column: 2;
+    margin-left: 1.5em;
+  }
+
+  p.italics {
+    font-style: italic;
+  }
+</style>
--- src/menus/Datasets.svelte
+++ src/menus/Datasets.svelte
@@ -1,5 +1,6 @@
 <script>
   import DatasetPicker from '../components/datasets/DatasetPicker.svelte';
+  import DatasetInfo from '../components/datasets/DatasetInfo.svelte';
 
   export let date;
   export let gDatasets;
@@ -21,38 +22,10 @@
 />
 </details>
 <details open>
-<summary><h2>Climate Information</h2></summary>
-<p>Temperature and precipitation averages (<b>avg.</b>) are over a monthly
-  period.
-<p>The <b>anomaly</b> is the difference between the average for a specific
-  month, e.g. August 2022, compared to an average over that same month from
-  1991–2020, e.g. all the months of August during 1991–2020.
-
-<p><b>Permafrost probability</b> is how likely that the average annual ground
-  temperature is below 0&nbsp;°C. Based on this value, permafrost zones are
-  classified as follows:
-  <dl>
-    <dt>&gt;0–10%<dd>isolated patches
-    <dt>&gt;10–50%<dd>sporadic
-    <dt>&gt;50–90%<dd>discontinuous
-    <dt>&gt;90–100%<dd>continuous
-  </dl>
+<summary><h2>About the selected dataset</h2></summary>
+<DatasetInfo
+  {griddedDataset}
+/>
 </details>
 
-<style>
-  dl {
-    display: grid;
-    grid-template-columns: max-content 1fr;
-  }
 
-  dt {
-    font-weight: bold;
-    text-align: right;
-    grid-column: 1;
-  }
-
-  dd {
-    grid-column: 2;
-    margin-left: 1.5em;
-  }
-</style>
