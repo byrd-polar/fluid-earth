ef6bb746d5395782c3893aab34d279858d8ee3cc
Em Zhan <zqianem@gmail.com>
2023-06-05 18:58:10 -0500

    Make OSCAR v2.0-based data visible
    Will require a few days for both the old and new sources to update their
    respective dataset names. Shouldn't be any issue if for a few days, the
    datasets have the same name; one of them should consistently take
    precedence.

--- backend/datasets/oscar-velocity.js
+++ backend/datasets/oscar-velocity.js
@@ -1,4 +1,4 @@
-export const name = 'ocean surface currents';
+export const name = '[v1] ocean surface currents';
 
 export const metadata = {
   particleLifetime: 10000,
--- backend/datasets/oscar2-velocity.js
+++ backend/datasets/oscar2-velocity.js
@@ -1,3 +1,3 @@
-export const name = 'ocean surface currents v2';
+export const name = 'ocean surface currents';
 
 export { metadata, convert } from './oscar-velocity.js';
